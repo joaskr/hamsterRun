1974ffb410bd765bd34d6789ab2f8b9b1c1c9e4b
joaskr <j.skrzypczak1998@gmail.com>
2024-06-02 20:42:45 +0200

    Added moving between lanes

--- Assets/Scripts/controlPlayer.cs
+++ Assets/Scripts/controlPlayer.cs
@@ -8,6 +8,11 @@ public class controlPlayer : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed;
+
+    // There are 3 lanes left (0), middle (1) and right (2)
+    private int actualLane = 1;
+    public float distanceBetweenLanes = 4;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,6 +22,26 @@ public class controlPlayer : MonoBehaviour
     void Update()
     {
         direction.z = forwardSpeed;
+        if(Input.GetKeyDown(KeyCode.RightArrow) & actualLane < 2)
+        {
+          actualLane++;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) & actualLane > 0)
+        {
+          actualLane--;
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+       
+        if(actualLane == 0)
+        {
+            targetPosition += Vector3.left * distanceBetweenLanes;
+        } else if (actualLane == 2)
+        {
+            targetPosition += Vector3.right * distanceBetweenLanes;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 60 * Time.fixedDeltaTime);
     }
 
     private void FixedUpdate()
